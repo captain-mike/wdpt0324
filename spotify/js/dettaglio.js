a4dbe09a90367b16ee931f001917acb3676dcc6f
captain-mike <info@grafikweb.it>
2024-08-21 21:03:39 +0200

    recap

--- spotify/js/dettaglio.js
+++ spotify/js/dettaglio.js
@@ -1,34 +1,54 @@
+import { Loader } from './loader.js'
+
+
 window.onload = function(){
+
+    const player = document.querySelector('#player')
 
     async function call(){
 
         //L'utente che atterra in questa pagina lo farà dopo aver cliccato una delle card della pagina index.html, di conseguenza all'interno della barra degli indirizzi, sarà disponibile un query param chiamato album a cui è assegnato l'ID di un album.
         const url = new URLSearchParams(location.search);//Utilizziamo URLSearchParams per poter leggere e manipolare i valori legati ai query params. 
+        if(!url.has('album')) {
+            location.href = '/'
+            return
+        }
 
         const id = url.get('album');//Recupero dall'url il valore legato al parametro album (L'ID dell'album )
 
+        Loader.showLoader()
+
         const response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${id}`);//Faccio una chiamata all'endpoint specifico che mi permette di recuperare un singolo album, fornendo anche un id. 
-        const dati = await response.json();//Converto la response in json. 
+        const dati = await response.json();//Converto la response in json.
+        
+        Loader.hideLoader()
         
         //Da qui in poi il codice effettua operazioni simili a quelle nel file Index JS. 
         let temp = document.getElementsByTagName("template")[0];
         let clone = temp.content.cloneNode(true);
+        
 
         const image = clone.querySelector('.image')
         const trackName = clone.querySelector('.track-name')
         const albumName = clone.querySelector('.album-name')
+        const play = clone.querySelector('.play')
 
         image.src = dati.cover_medium
         albumName.textContent = dati.title
         trackName.textContent = dati.tracks.data[0].title
 
+        play.addEventListener('click',()=>{
+            player.src = dati.tracks.data[0].preview
+            player.load()
+            console.log(player.playing);
+            
+            player.play()
 
-        console.log(dati);
+        })
+
+        target.append(clone)
+
         
-
-        
-
-
     }
 
     call()
--- spotify/js/index.js
+++ spotify/js/index.js
@@ -1,10 +1,15 @@
+import { Loader } from './loader.js'
+
 window.onload = () => {
 
 // Utilizziamo una funzione asincrona in modo da poter gestire in maniera semplice e con una sintassi facile da leggere le operazioni asincrone legate alla fecth
     async function call(){
+        Loader.showLoader()
         //La parola chiave await mi permette di far sì che ci sia un'attesa che termina quando l'esecuzione asincrona è terminata 
         const response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=queen`)//Salvo nella variabile response, l'oggetto di tipo response restituito dalla chiamata fetch. 
         const risultato = await response.json()//Una volta ottenuta la response, La converto in json, ottenendo di fatto il contenuto dell'endpoint (I dati che devo utilizzare per costruire le card )
+
+        Loader.hideLoader()
 
         const target = document.querySelector('#target')//Cerco l'elemento con ID target, ossia l'area in cui costruirò le card. 
 
@@ -14,12 +19,21 @@ window.onload = () => {
 
             let temp = document.getElementsByTagName("template")[0];//Cerco il tag template presente in pagina. 
             let clone = temp.content.cloneNode(true);//Crea un clone del contenuto del tag template. 
+            
+            //Conversione del document fragment in un elemento DOM. 
+            const div = document.createElement("div");
+            div.append(clone);//Inserisco il clone in un DIV appena creato. 
+            clone = div.firstElementChild//Lo estrapolo dal suddetto DIV e lo reinserisco nella variabile clone. 
+
+            console.log(clone);//Come puoi vedere adesso clone non è più un document fragment 
+            
 
             //Nella variabile clone ora abbiamo degli elementi HTML sotto forma di oggetti del DOM, di conseguenza posso usare querySelector direttamente sulla variabile clone per cercare le porzioni di codice che voglio manipolare 
             const artistLink = clone.querySelector('.artist-link')
             const image = clone.querySelector('.image')
             const albumName = clone.querySelector('.album-name')
             const artistName = clone.querySelector('.artist-name')
+            const button = clone.querySelector('button')
 
 
             //Dopo aver selezionato tutti gli elementi da manipolare, procedo a scrivere al loro interno o ad assegnare valori ai loro attributi 
@@ -27,6 +41,10 @@ window.onload = () => {
             image.src = el.album.cover_medium
             albumName.textContent = el.album.title
             artistName.textContent = el.artist.name
+
+            button.addEventListener('click',()=>{
+                button.parentElement.remove()
+            })
 
             target.appendChild(clone);//Inserisco nella pagina la card appena creata 
 
